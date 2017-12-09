880c4f52d1071ebc8eb0507e664d3acb3b99a8f4
jmhe <jmhe@Ctrip.com>
2017-12-09 16:28:29 +0800

    注释

--- RACDemo/ViewController.m
+++ RACDemo/ViewController.m
@@ -18,32 +18,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    RACSequence *sequence1 = [RACSequence sequenceWithHeadBlock:^id _Nullable{
-        return @1;
-    } tailBlock:^RACSequence * _Nonnull{
-        return [RACSequence return:@2];
-    }];
-    
-    RACSequence *sequence2 = [RACSequence sequenceWithHeadBlock:^id _Nullable{
-        return @3;
-    } tailBlock:^RACSequence * _Nonnull{
-        return [RACSequence return:@4];
-    }];
-    
-     RACSequence *seq = [[RACArraySequence sequenceWithArray:@[sequence1, sequence2] offset:0] bind:^{
-        return ^(id value, BOOL *stop) {
-            return value;
-        };
-    }];
-    
-    
-    id current;
-    NSEnumerator *en = seq.objectEnumerator;
-    while (current =  [en nextObject]) {
-        NSLog(@"%@",current);
-    }
-    
 }
 
 @end
